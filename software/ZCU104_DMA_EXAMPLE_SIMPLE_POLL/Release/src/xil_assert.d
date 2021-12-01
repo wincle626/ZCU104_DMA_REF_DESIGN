d9583e07764d9c63b9aa26bb041619b12c8ec365
Yun Wu <vvface@gmail.com>
2021-12-01 16:26:31 +0000

    add files

--- software/ZCU104_DMA_EXAMPLE_SIMPLE_POLL/Release/src/xil_assert.d
+++
@@ -1,6 +0,0 @@
-src/xil_assert.o src/xil_assert.o: ../src/xil_assert.c ../src/xil_types.h \
- ../src/xil_assert.h
-
-../src/xil_types.h:
-
-../src/xil_assert.h:
---
+++ software/ZCU104_DMA_EXAMPLE_SIMPLE_POLL_32BIT/Release/src/xaxidma_bdring.d
@@ -0,0 +1,39 @@
+src/xaxidma_bdring.o src/xaxidma_bdring.o: ../src/xaxidma_bdring.c \
+ ../src/xaxidma_bdring.h ../src/xstatus.h ../src/xil_types.h \
+ ../src/xil_assert.h ../src/xaxidma_bd.h ../src/xaxidma_hw.h \
+ ../src/xil_io.h ../src/xil_printf.h ../src/xparameters.h \
+ ../src/xparameters_ps.h ../src/bspconfig.h ../src/xpseudo_asm.h \
+ ../src/xreg_cortexa53.h ../src/xpseudo_asm_gcc.h ../src/xdebug.h \
+ ../src/xil_cache.h
+
+../src/xaxidma_bdring.h:
+
+../src/xstatus.h:
+
+../src/xil_types.h:
+
+../src/xil_assert.h:
+
+../src/xaxidma_bd.h:
+
+../src/xaxidma_hw.h:
+
+../src/xil_io.h:
+
+../src/xil_printf.h:
+
+../src/xparameters.h:
+
+../src/xparameters_ps.h:
+
+../src/bspconfig.h:
+
+../src/xpseudo_asm.h:
+
+../src/xreg_cortexa53.h:
+
+../src/xpseudo_asm_gcc.h:
+
+../src/xdebug.h:
+
+../src/xil_cache.h:

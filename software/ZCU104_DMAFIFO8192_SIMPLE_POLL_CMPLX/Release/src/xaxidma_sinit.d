560c668d985e4a285383cdce80f13ac7f15361f6
Yun Wu <vvface@gmail.com>
2022-01-11 17:20:45 +0000

    add fifo 8192 complex stream

---
+++ software/ZCU104_DMAFIFO8192_SIMPLE_POLL_CMPLX/Release/src/xaxidma_sinit.d
@@ -0,0 +1,41 @@
+src/xaxidma_sinit.o src/xaxidma_sinit.o: ../src/xaxidma_sinit.c \
+ ../src/xparameters.h ../src/xparameters_ps.h ../src/xaxidma.h \
+ ../src/xaxidma_bdring.h ../src/xstatus.h ../src/xil_types.h \
+ ../src/xil_assert.h ../src/xaxidma_bd.h ../src/xaxidma_hw.h \
+ ../src/xil_io.h ../src/xil_printf.h ../src/bspconfig.h \
+ ../src/xpseudo_asm.h ../src/xreg_cortexa53.h ../src/xpseudo_asm_gcc.h \
+ ../src/xdebug.h ../src/xil_cache.h
+
+../src/xparameters.h:
+
+../src/xparameters_ps.h:
+
+../src/xaxidma.h:
+
+../src/xaxidma_bdring.h:
+
+../src/xstatus.h:
+
+../src/xil_types.h:
+
+../src/xil_assert.h:
+
+../src/xaxidma_bd.h:
+
+../src/xaxidma_hw.h:
+
+../src/xil_io.h:
+
+../src/xil_printf.h:
+
+../src/bspconfig.h:
+
+../src/xpseudo_asm.h:
+
+../src/xreg_cortexa53.h:
+
+../src/xpseudo_asm_gcc.h:
+
+../src/xdebug.h:
+
+../src/xil_cache.h:
---
+++ software/ZCU104_DMAFIFO8192_SIMPLE_POLL_CMPLX/Release/src/xil_io.d
@@ -0,0 +1,24 @@
+src/xil_io.o src/xil_io.o: ../src/xil_io.c ../src/xil_io.h \
+ ../src/xil_types.h ../src/xil_printf.h ../src/xparameters.h \
+ ../src/xparameters_ps.h ../src/bspconfig.h ../src/xpseudo_asm.h \
+ ../src/xreg_cortexa53.h ../src/xpseudo_asm_gcc.h ../src/xil_assert.h
+
+../src/xil_io.h:
+
+../src/xil_types.h:
+
+../src/xil_printf.h:
+
+../src/xparameters.h:
+
+../src/xparameters_ps.h:
+
+../src/bspconfig.h:
+
+../src/xpseudo_asm.h:
+
+../src/xreg_cortexa53.h:
+
+../src/xpseudo_asm_gcc.h:
+
+../src/xil_assert.h:
